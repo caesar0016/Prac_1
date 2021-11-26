86e8b26e9d272a6e6406740dbb4d90e88a0eef70
caesar0016 <dapitoncaesar26@gmail.com>
2021-11-26 16:31:14 +0800

    savings_system
    My first program.

---
+++ JavaApplication13/src/javaapplication13/JavaApplication13.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication13;
+
+
+
+
+
+/**
+ *
+ * @author Caesar
+ */
+public class JavaApplication13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int num1 = (10);
+        int num2 = (5);
+        
+        if (num1 == num2){
+        System.out.println("Equal");
+        }
+        
+    }
+    
+}
---
+++ test 2/src/test/pkg2/Test2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.pkg2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Caesar
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    int numbers [] = {525, 2658, 873, 2564, 5789, 54546,};
+    
+    System.out.println(numbers[4]);
+        
+        label = output
+        text field = input
+        
+    }
+    
+}
