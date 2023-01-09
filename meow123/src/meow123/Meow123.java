6d6626fe8a0cdbe682f51584fdc69ea5d96548bf
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2023-01-09 08:08:44 +0800

    Frameskip
    While thinking about the UI and was bored. I was able to think about this framing

---
+++ meow123/src/meow123/Meow123.java
@@ -0,0 +1,215 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package meow123;
+
+/**
+ *
+ * @author STI
+ */
+public class Meow123 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        while(true){
+            
+            System.out.println("..|||..");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            
+            
+            System.out.println("....");
+            System.out.println("     ..|||..");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ..|||..");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ..|||..");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ..|||..");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ..|||..");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ..|||..");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ..|||..");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ..|||..");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ..|||..");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ..|||..");
+            System.out.println("     ....");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ..|||..");
+            System.out.println("....");
+            
+            System.out.println("....");
+            System.out.println("     ....");
+            System.out.println("         ....");
+            System.out.println("             ....");
+            System.out.println("                 ....");
+            System.out.println("                     ....");
+            System.out.println("                         ....");
+            System.out.println("                     ....");
+            System.out.println("                 ....");
+            System.out.println("             ....");
+            System.out.println("         ....");
+            System.out.println("     ....");
+            System.out.println("..|||..");
+            
+
+            
+            
+            
+            
+            
+        
+        }
+        
+    }
+    
+}
